9b914f761329f9da9bac51e634ab549ce2cb6eec
Heather Runyon <hrocco25@gmail.COM>
2020-01-27 10:33:39 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Main from "./pages/main/main"
-
+import Header from "./component/header/header"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Main />
       </div>
     );
