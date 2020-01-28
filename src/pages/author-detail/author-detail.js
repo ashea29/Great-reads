91290c0b01356db3611d0dd5047b0fe0270c6c08
Heather Runyon <hrocco25@gmail.COM>
2020-01-28 14:58:50 -0700

    author detail working

--- src/pages/author-detail/author-detail.js
+++ src/pages/author-detail/author-detail.js
@@ -2,15 +2,41 @@ import React, { Component } from "react"
 import "./author-detail.css"
 
 class AuthorDetail extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    }
+  }
+
+
+
+  componentDidMount() {
+    const { name }= this.props.match.params
+    fetch(`https://great-reads-seir1118.herokuapp.com/authors/byName/${name}`)
+      .then(res => res.json())
+      .then(res => this.setState({ books: res.books }))
+  }
+
+
   
   render(){
-
-    
+    if(this.state.books.length !== 0){
     return(
-      <div>
-        <h1>detail</h1>
+      <div className="authors-book">
+        <h1>{this.props.match.params.name}</h1>
+        {this.state.books.map(data =>
+          <div key={data._id}>
+            <h3>{data.title}</h3>
+            <p>{data.description}</p>
+          </div>
+       )}
       </div>
     )
+    }else{
+      return(<div></div>)
+    }
   }
 }
 
