0324a7805845c07960c1f0cdc56929555d3fa1a3
VyLe <thaovyletruong@gmail.com>
2020-01-29 12:48:21 -0600

    adding link to detail author function

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -6,15 +6,20 @@ const Main = (props) => {
   return (
     <div className="bookdisplay">
       {props.books.map(eachBook =>
-        <div key={eachBook._id} className="eachbookholder" onClick={(e) => props.bookIdHandle(e)} >
-          <Link to={`/book/${eachBook._id}`}><section id={eachBook._id} className="bookdetaillinksection"></section></Link>
+        <div key={eachBook._id} className="eachbookholder"  >
+
           <section id={eachBook._id} onClick={(e) => props.bookIdHandle(e)} style={{ backgroundImage: `url(${eachBook.coverImgURL})` }} className="eachbookimage">
+            <div onClick={(e) => props.bookIdHandle(e)}>
+              <Link to={`/book/${eachBook._id}`}><section id={eachBook._id} className="bookdetaillinksection">  </section></Link>
+            </div>
             <small className="bookdelete" id={eachBook._id} onClick={(e) => props.bookIdHandle(e)}>Delete</small>
             <small className="bookedit" id={eachBook._id} onClick={(e) => props.bookIdHandle(e)}>Edit</small>
           </section>
           <section>
             <p className="eachbooktitle">{eachBook.title}</p>
-            <p className="eachbookauthor">{eachBook.author.name}</p>
+            <Link to={`/author/${eachBook.author.name}`} className="eachbookauthorholder">
+              <p className="eachbookauthor" >{eachBook.author.name}</p>
+            </Link>
           </section>
         </div>
       )}
