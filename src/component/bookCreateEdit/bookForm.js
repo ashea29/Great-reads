6210b2aaeb03289b9982546f011bbaeccb11a319
VyLe <thaovyletruong@gmail.com>
2020-01-30 14:23:05 -0600

    display author input none in edit pop up

--- src/component/bookCreateEdit/bookForm.js
+++ src/component/bookCreateEdit/bookForm.js
@@ -10,8 +10,8 @@ const BookForm = (props) => {
         <span className="esc" onClick={props.escHandle}>X</span>
         <h3>{bookFormHeader}</h3>
         <form action="" className="bookinputholder">
-          <input type="text" className="bookname" placeholder="Book's Name" name="title" onChange={e => props.inputHandle(e)} required />
-          <input type="text" className="bookauthor" placeholder="Book's Author" name="author" onChange={e => props.inputHandle(e)} required />
+          <input type="text" className={props.bookAction === "create" ? "bookname" : "bookeditname"} placeholder="Book's Name" name="title" onChange={e => props.inputHandle(e)} required />
+          {props.bookAction === "create" ? <input type="text" className="bookauthor" placeholder="Book's Author" name="author" onChange={e => props.inputHandle(e)} required /> : <p></p>}
           <input type="url" className="bookurl" placeholder="Book Cover Url" name="url" onChange={e => props.inputHandle(e)} required />
           <textarea type="text" className="bookdetail" placeholder="...Detail" name="detail" onChange={e => props.inputHandle(e)} required />
           <button className="submitbutton" onClick={e => props.bookSubmitHandle(e)}>Submit</button>
