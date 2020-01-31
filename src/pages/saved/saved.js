46ff4be6d66826be8e4a12766da13acebccadbab
VyLe <thaovyletruong@gmail.com>
2020-01-31 11:59:21 -0600

    finished fixing locale storage bug

--- src/pages/saved/saved.js
+++ src/pages/saved/saved.js
@@ -3,63 +3,34 @@ import { Link } from "react-router-dom"
 import "./saved.css"
 import "../book-detail/book-detail.css"
 
-class Saved extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      savedBook: [],
-      deletedBook: ''
-    }
-  }
-  componentDidMount() {
-    const fetchId = JSON.parse(localStorage.getItem('savedBookId')) || []
-    this.setState({ savedBook: fetchId })
-  }
-  removeHandle = (e) => {
-    const newFetchId = this.state.savedBook.concat()
-    const removedIndex = e.target.attributes.getNamedItem('index').value
-    newFetchId.splice(removedIndex, 1)
-    localStorage.setItem('savedBookId', JSON.stringify(newFetchId))
-    this.setState({ savedBook: newFetchId })
-  }
-  render() {
-    return (
-      <div className="savedbook">
-        <h1>My favourite book(s)</h1>
-        <div className="bookdisplay">
-          {this.state.savedBook.map((eachBook, i) =>
-            <div key={eachBook._id} className="eachbookholder"  >
-
-              <section style={{ backgroundImage: `url(${eachBook.coverImgURL})` }} className="eachbookimage">
-                <div id={eachBook._id} onClick={(e) => this.props.bookIdHandle(e)}>
-                  <Link to={`/book/${eachBook._id}`}><section id={eachBook._id} className="bookdetaillinksection">  </section></Link>
-                </div>
-                <small className="bookdelete" index={i} onClick={e => this.removeHandle(e)}>Remove</small>
-              </section>
-              <section>
-                <Link to={`/book/${eachBook._id}`} className="eachbooktitleholder">
-                  <p className="eachbooktitle">{eachBook.title}</p>
-                </Link>
-                <Link to={`/author/${eachBook.author}`} className="eachbookauthorholder">
-                  <p className="eachbookauthor" >{eachBook.author}</p>
-                </Link>
-              </section>
-            </div>
-          )}
-        </div>
-      </div >
-    )
-    // } else {
-    //   return (
-    //     <div>
-    //       <br /> <br /> <br /> <br /> <br /> <br />
-    //       <br /> <br /> <br /> <br /> <br /> <br />
-    //       <h1> Loading... </h1>
-    //     </div>
-    //   )
-    // }
-
-  }
+const Saved = (props) => {
+  return (
+    <div className="savedbook">
+      <h1>My favourite book(s)</h1>
+      <div className="bookdisplay">
+        {props.savedBookId.map((eachBook, i) =>
+          <div key={eachBook._id} className="eachbookholder">
+            <section style={{ backgroundImage: `url(${eachBook.coverImgURL})` }} className="eachbookimage">
+              <div id={eachBook._id} onClick={(e) => props.bookIdHandle(e)}>
+                <Link to={`/book/${eachBook._id}`}><section id={eachBook._id} className="bookdetaillinksection">  </section></Link>
+              </div>
+              <small className="bookdelete" index={i} onClick={e => props.removeHandle(e)}>Remove</small>
+            </section>
+            <section>
+              <Link to={`/book/${eachBook._id}`} className="eachbooktitleholder">
+                <p className="eachbooktitle">{eachBook.title}</p>
+              </Link>
+              <Link to={`/author/${eachBook.author}`} className="eachbookauthorholder">
+                <p className="eachbookauthor" >{eachBook.author}</p>
+              </Link>
+            </section>
+          </div>
+        )}
+      </div>
+    </div >
+  )
 }
+
+
 
 export default Saved
